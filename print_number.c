aa8a53f0a808293b49099dec133ab0c504234588
abdelrahman-2513 <ab19622001@gmail.com>
2023-08-19 06:22:22 -0700

    comit

---
+++ print_number.c
@@ -0,0 +1,84 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if the char is digit
+ * @c: the character toi be checked
+ *
+ * Return: 1 if digit, 0 if else
+ */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9')
+}
+
+/**
+ * _strlen - return the length of string
+ * @s: the string to get the length of
+ *
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * print_number - print a number with options
+ * @str: the base as strimg
+ * @params: the param struct
+ *
+ * Return: chars printed
+ */
+int print_number(char *str, params_t params)
+{
+	unsigned int i = _strlen(str);
+	int neg = (!params->unsign && *str == '-');
+
+	if (!params->percision && *str == '0' && !str[1])
+		str = "";
+	if (neg)
+	{
+		str++;
+		i--;
+	}
+	if (params->percision != UINT_MAX)
+		while (i++ < params->percision)
+			*--str = '0';
+	if (neg)
+		*--str = '-';
+	if (!params->minus_flag)
+		return (print_number_right_shift(str, params));
+	else
+		return (print_number_left_shift(str, params));
+}
+
+/**
+ * print_number_right_shift - prints a number with shift
+ * @str: the base number as string
+ * @params: the paramter of struct
+ *
+ * Return: chars printed
+ */
+int print_number_right_shift(char *str, params_t params)
+{
+	unsigned int n = 0, neg, neg2, i = _strlen(str);
+	char pad_char = '';
+	
+	if (params->zero_flag && !params->minusflag)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *str =='-')
+	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+		str++;
+	else
+		neg = 0;
+	if ((params->plus_flag && !neg2) || if (!params->plus_flag && !neg2 && params->space_flag))
+		i++;
+	if (neg && pad_char = '0')
+		n += _putchar('-');
+	while (i++ < params->width)
+		n += _putchar(pad_char);
+
