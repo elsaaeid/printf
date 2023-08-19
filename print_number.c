46846b0e997a2bfc3fda559b2d43bfa6404629f4
abdelrahman-2513 <ab19622001@gmail.com>
2023-08-19 07:11:44 -0700

    print_number.c

--- print_number.c
+++ print_number.c
@@ -8,7 +8,7 @@
  */
 int _isdigit(int c)
 {
-	return (c >= '0' && c <= '9')
+	return (c >= '0' && c <= '9');
 }
 
 /**
@@ -67,18 +67,61 @@ int print_number_right_shift(char *str, params_t params)
 {
 	unsigned int n = 0, neg, neg2, i = _strlen(str);
 	char pad_char = '';
-	
+
 	if (params->zero_flag && !params->minusflag)
 		pad_char = '0';
-	neg = neg2 = (!params->unsign && *str =='-')
+	neg = neg2 = (!params->unsign && *str == '-')
 	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
 		str++;
 	else
 		neg = 0;
-	if ((params->plus_flag && !neg2) || if (!params->plus_flag && !neg2 && params->space_flag))
+	if ((params->plus_flag && !neg2) ||
+			(!params->plus_flag && !neg2 && params->space_flag))
 		i++;
-	if (neg && pad_char = '0')
+	if (neg && pad_char == '0')
 		n += _putchar('-');
+	if (params->plus_flag && !neg2 && pad_char == '0' && !params->unsign)
+		n += _putchar('+');
+	else if (!params->plus_flag && !neg2 && params->space_flag &&
+			!params->unsign && params->zero_flag)
 	while (i++ < params->width)
 		n += _putchar(pad_char);
+	if (neg && pad_char == ' ')
+		n += _putchar('-');
+	if (params->plus_flag && !neg2 && !params->unsign)
+		n += _putchar('+');
+	else if (!params->plus_flag && !neg2 && params->space_flag &&
+		       !params->unsign && !params->zero_flag)
+		n += _putchar(' ');
+	n += _puts(str);
+	return (n);
+}
 
+/**
+ * print_number_left_shift - prints a number with shift
+ * @str: the base number as string
+ * @params: the paramter of struct
+ *
+ * Return: chars printed
+ */
+int print_number_left_shift(char *str, params_t params)
+{
+	unsigned int n = 0, neg, neg2, i = _strlen(str);
+	char pad_char = '';
+
+	if (params->zero_flag && !params->minusflag)
+		pad_char = '0';
+	neg = neg2 = (!params->unsign && *str == '-')
+	if (neg && i < params->width && pad_char == '0' && !params->minus_flag)
+		str++;
+	else
+		neg = 0;
+	if (params->plus_flag && !neg2 && !params->unsign)
+		n += _putchar('+'), i++;
+	else if (!neg2 && !params->unsign && params->space_flag)
+		n += _putchar(' '), i++;
+	n += _puts(str);
+	while (i++ < params->width)
+		n += _putchar(pad_char);
+	return (n);
+}
